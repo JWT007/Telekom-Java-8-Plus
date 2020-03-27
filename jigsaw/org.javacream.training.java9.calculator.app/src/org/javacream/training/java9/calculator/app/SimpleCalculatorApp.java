c2dd0e7f4dcccb1be40d4783bab7be6fb00d3872
Rainer Sawitzki <training@rainer-sawitzki.de>
2020-03-27 14:03:09 +0100

    add classlibs example

--- jigsaw/org.javacream.training.java9.calculator.app/src/org/javacream/training/java9/calculator/app/SimpleCalculatorApp.java
+++ jigsaw/org.javacream.training.java9.calculator.app/src/org/javacream/training/java9/calculator/app/SimpleCalculatorApp.java
@@ -2,7 +2,6 @@ package org.javacream.training.java9.calculator.app;
 
 import org.javacream.training.java9.calculator.util.Calculator;
 
-
 public class SimpleCalculatorApp {
 
 	public static void main(String[] args) {
--- jigsaw/org.javacream.training.java9.calculator.util/src/module-info.java
+++ jigsaw/org.javacream.training.java9.calculator.util/src/module-info.java
@@ -7,4 +7,5 @@
  */
 module org.javacream.training.java9.calculator.util {
 	exports org.javacream.training.java9.calculator.util;
+	
 }
